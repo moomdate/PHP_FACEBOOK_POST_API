d69a284cd0e9bfced75881417ae7de55de757a67
Surasak sincharoen <moomdate@gmail.com>
2017-02-24 09:24:46 +0700

    Facebook Post Create Class
    Facebook Post API Function

---
+++ FacebookPost.php
@@ -0,0 +1,31 @@
+<?php
+class FacebookFunc{
+	private $accessToken;
+	public function __construct($accessToken){
+		$this->accessToken = $accessToken;
+	}
+	private function curl_get_file_contents($URL){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $URL);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$contents = curl_exec($ch);
+		curl_close($ch);
+		
+		if($contents){
+			return $contents;
+		}else{
+			return false;
+		}
+	}
+	private function getAccessToken(){
+		return $this->accessToken;
+	}
+	public function postMessage($message){
+		$url = "https://graph.facebook.com/me/feed?method=POST&message=".$message."&access_token=".$this->getAccessToken();
+		$response = $this->curl_get_file_contents($url);
+		$result = json_decode($response,true);
+		return $result;
+	}
+}
+?>
